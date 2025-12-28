5f0c5f2c7f43535c423080935d12d33b022c5281
aleidhussain <aleidhussain202@gmail.com>
2025-12-28 16:51:31 +0300

    Initialize project with starter files

---
+++ script.js
@@ -0,0 +1,9 @@
+// TODO: Write your JavaScript logic here!
+
+// 1. Select your elements (input, button, list) e.g. document.getElementById('...')
+
+// 2. Add an event listener to the button for 'click'
+
+// 3. Keep track of tasks (maybe in an array?)
+
+// 4. Create a function to render the tasks to the screen
